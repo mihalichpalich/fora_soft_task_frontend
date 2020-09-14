623679256d13e18b2b8af14cbf64b8f0ae055e74
mihalichpalich <pavlovtfspb@gmail.com>
2020-09-14 16:28:03 +0300

    Updates

---
+++ precache-manifest.bf5d427b361f442d0e7125ae4b54223b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d38807c6d53c084049b9968fdf937b",
+    "url": "/fora_soft_task_frontend/index.html"
+  },
+  {
+    "revision": "18a8e199020c18ac1bac",
+    "url": "/fora_soft_task_frontend/static/css/main.72994cf6.chunk.css"
+  },
+  {
+    "revision": "ba18ecb74b6a584631dd",
+    "url": "/fora_soft_task_frontend/static/js/2.69db739a.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/fora_soft_task_frontend/static/js/2.69db739a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a8e199020c18ac1bac",
+    "url": "/fora_soft_task_frontend/static/js/main.4db64eda.chunk.js"
+  },
+  {
+    "revision": "b9142c7b8dfd8ab10977",
+    "url": "/fora_soft_task_frontend/static/js/runtime-main.dc250800.js"
+  }
+]);
